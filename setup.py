5891d89d0a8167001af5eb9730c95d96c73d14f7
dfguerrerom <dfgm2006@gmail.com>
2024-04-22 13:12:10 +0200

    build: fix installation

--- setup.py
+++ setup.py
@@ -34,7 +34,16 @@ class DevelopCmd(develop):
         super(DevelopCmd, self).run()
 
 
+data_files = []
+# Add all the templates
+for dirpath, dirnames, filenames in os.walk("share/jupyter/"):
+    if filenames:
+        data_files.append(
+            (dirpath, [os.path.join(dirpath, filename) for filename in filenames])
+        )
+
 setup(
+    data_files=data_files,
     cmdclass={
         "develop": DevelopCmd,
     },
